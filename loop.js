7676103e9b797e000401de86f571c4f8196aaf6e
jiya349 <narangjiya38@gmail.com>
2026-09-09 09:03:06 +0530

    hello

---
+++ loop.js
@@ -0,0 +1,171 @@
+// loops
+// let i = 10;
+// while(true){
+//     console.log(i)
+//     i+=10;
+//     if(i==100){
+//         break;
+//     }
+// }
+
+// let i = 19;
+// while(i<=190){
+//     console.log(i);
+//     i+=19;
+// }
+
+// let str = "Kanwarpal Singh(lassi nhi lata) and Jiya Narang(Rang nhi hai jiwan me) and Krishna(jisme bhagwan wale koi gun nhi hai)"
+// let i = 110;
+// while(i<str.length){
+//     console.log(str[i]);
+//     i++;
+// }
+
+// let a = 10;
+// let b = ++a;
+// console.log(a)
+// console.log(b)
+
+// let target = "Jiya";
+// let arr = str.split(" ")
+// console.log(arr)
+// while(i<arr.length){
+//     if(arr[i]==target){
+//         console.log("target found",i,arr[i])
+//     }
+//     i++;
+// }
+// console.log(str.length)
+
+// do{
+//     console.log("Traversing the string...");
+//     console.log(str[i])
+//     i++;
+// }while(i<str.length)
+
+
+//for loop
+//initialization
+// let v = 10
+
+// for(let i=10;i>0 ;i--){
+//     console.log(i)
+// }
+
+// let arr = [1,2,3,4,5,6,7]
+// for(let i=0;i<=arr.length;i++){
+//     console.log(arr[i])
+// }
+// let str = "Hello how are you?"
+// for(let i = 0;i<str.length;i++){
+//     console.log(str[i])
+// }
+
+
+// let target = 6
+// for(let i=0; i<arr.length;i++){
+//     if(arr[i]==target){
+//         console.log("target found at index ",i)
+//     }
+// }
+
+
+//arrays
+// let str1 = "abcd"
+// let str2 = "abcd"
+
+// let num = 2
+// let str3 = "2"
+// const arr = [1,2,3,4]
+// let arr2 = [1,2,3,4]
+// arr[1] = 2
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+// // console.log(num===str3)
+// // console.log(str1==str2)
+// // console.log(arr==arr2)
+
+// // let arr3 = arr;
+
+// // console.log(arr === arr3)
+
+// arr.shift() 
+// // arr.push(6)
+// arr.pop()
+// console.log(arr)
+// let array = [2,3,54,6,10,11,534]
+// let target = 10
+
+// for(let i = 0; i<array.length; i++){
+//     if(array[i]==target){
+//         console.log("target found at index",i)
+//         break
+//     }
+// }
+
+
+// let arr = [1,2,4,[2,4,6,7],[8,3,5,7]]
+
+// console.log(arr[2][2])
+
+//objects
+
+//key:value
+// let obj = {
+//     name:"Batman",
+//     age:23,
+//     address:{
+//         state:"haryana",
+//         city:"Kaithal",
+//         street_add:"pata nhi",
+//         pincode:136027
+//     }
+// }
+// // console.log(obj.address.city)
+
+// let data = [{
+//     name:"Batman",
+//     age:23,
+//     address:{
+//         state:"haryana",
+//         city:"Kaithal",
+//         street_add:"pata nhi",
+//         pincode:136027
+//     }
+// },{
+//     name:"Superman",
+//     age:22,
+//     address:{
+//         state:"haryana",
+//         city:"Kaithal",
+//         street_add:"pata nhi",
+//         pincode:136027
+//     }
+// }]
+
+// console.log(data[1].address.pincode)
+
+
+
+// let arr = [9,5,8,3,78,2,75]
+
+// let max = arr[0]
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
+
+// let min = arr[0]
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<min){
+//         min = arr[i]
+//     }
+// }
+// console.log(min)
+
+
--- task.js
+++ task.js
@@ -1,97 +1,127 @@
-// Task 1
-let day = "Thursday"
-switch (day) {
-    case "Monday":
-        console.log("Work Day");
-        break;
-    case "Tuesday":
-        console.log("Work Day");
-        break;
-    case "Wednesday":
-        console.log("Work Day");
-        break;
-    case "Thursday":
-        console.log("Work Day");
-        break;
-    case "Friday":
-        console.log("Work Day");
-        break;
-    case "Saturday":
-        console.log("Weekend");
-        break;
-    case "Sunday":
-        console.log("Weekend");
-        break;
-    default:
-        console.log("Enter a valid day");
-}
-
-// Task 2
-// let month = prompt("Enter the month...")
-// switch (month) {
-//     case "January":
-//         console.log("Winter❄️");
+// // Task 1
+// let day = prompt("Enter the day...")
+// switch (day) {
+//     case "Monday":
+//         console.log("Work Day");
 //         break;
-//     case "February":
-//         console.log("Spring🌸");
+//     case "Tuesday":
+//         console.log("Work Day");
 //         break;
-//     case "March":
-//         console.log("Spring🌸");
+//     case "Wednesday":
+//         console.log("Work Day");
 //         break;
-//     case "April":
-//         console.log("Summer☀️");
+//     case "Thursday":
+//         console.log("Work Day");
 //         break;
-//     case "May":
-//         console.log("Summer☀️");
+//     case "Friday":
+//         console.log("Work Day");
 //         break;
-//     case "June":
-//         console.log("Summer☀️");
+//     case "Saturday":
+//         console.log("Weekend");
 //         break;
-//     case "July":
-//         console.log("Monsoon🌧️");
-//         break;
-//     case "August":
-//         console.log("Monsoon🌧️");
-//         break;
-//     case "September":
-//         console.log("Autumn🍂");
-//         break;
-//     case "October":
-//         console.log("Autumn🍂");
-//         break;
-//     case "November":
-//         console.log("Winter❄️");
-//         break;
-//     case "December":
-//         console.log("Winter❄️");
+//     case "Sunday":
+//         console.log("Weekend");
 //         break;
 //     default:
-//         console.log("Enter a valid month");
+//         console.log("Enter a valid day");
 // }
 
-// Task 3
-let num1 = parseInt(prompt("Enter number 1..."));
-let num2 = parseInt(prompt("Enter number 2..."));
-let symbol =prompt("Enter symbols...");
-switch (symbol) {
-    case "+":
-        console.log(num1 + num2);
-        break;
-    case "-":
-        console.log(num1 - num2);
-        break;
-    case "*":
-        console.log(num1 * num2);
-        break;
-    case "%":
-        console.log(num1 % num2);
-        break;
-    case "/":
-        console.log(num1 / num2);
-        break;
-    case "**":
-        console.log(Math.pow(num1,num2));
-        break;
-    default:
-        console.log("Enter a valid symbol");
+// // Task 2
+// // let month = prompt("Enter the month...")
+// // switch (month) {
+// //     case "January":
+// //         console.log("Winter❄️");
+// //         break;
+// //     case "February":
+// //         console.log("Spring🌸");
+// //         break;
+// //     case "March":
+// //         console.log("Spring🌸");
+// //         break;
+// //     case "April":
+// //         console.log("Summer☀️");
+// //         break;
+// //     case "May":
+// //         console.log("Summer☀️");
+// //         break;
+// //     case "June":
+// //         console.log("Summer☀️");
+// //         break;
+// //     case "July":
+// //         console.log("Monsoon🌧️");
+// //         break;
+// //     case "August":
+// //         console.log("Monsoon🌧️");
+// //         break;
+// //     case "September":
+// //         console.log("Autumn🍂");
+// //         break;
+// //     case "October":
+// //         console.log("Autumn🍂");
+// //         break;
+// //     case "November":
+// //         console.log("Winter❄️");
+// //         break;
+// //     case "December":
+// //         console.log("Winter❄️");
+// //         break;
+// //     default:
+// //         console.log("Enter a valid month");
+// // }
+
+// // Task 3
+// let num1 = parseInt(prompt("Enter number 1..."));
+// let num2 = parseInt(prompt("Enter number 2..."));
+// let symbol =prompt("Enter symbols...");
+// switch (symbol) {
+//     case "+":
+//         console.log(num1 + num2);
+//         break;
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+//     case "*":
+//         console.log(num1 * num2);
+//         break;
+//     case "%":
+//         console.log(num1 % num2);
+//         break;
+//     case "/":
+//         console.log(num1 / num2);
+//         break;
+//     case "**":
+//         console.log(Math.pow(num1,num2));
+//         break;
+//     default:
+//         console.log("Enter a valid symbol");
+// }
+
+// task 4
+
+// let i = 10; 
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// task 5
+// let num = parseInt(prompt("Enter a number"));
+// let i = 2;
+// while(i <= num){
+//     console.log(i);
+//     i+=2;
+// }
+
+// let num = parseInt(prompt("Enter a number"));
+// let sum = 0,i=1;
+
+// while(i<=num){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum)
+
+let arr = [1,2,3,4,5,6,7]
+for(let i=0;i<=arr.length;i++){
+    console.log(arr[i])
 }
